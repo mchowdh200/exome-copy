07c42463d29844b7949c939dc75ec96156bfcc0f
Ubuntu <ubuntu@ip-172-31-46-233.ec2.internal>
2019-02-11 22:57:33 +0000

    bam_processing should be good now *crosses fingers*

--- bam_processing.sh
+++ bam_processing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+script_dir=~/exome-copy
 storage_dir=/mnt/local
 
 # get file name from command line arg
@@ -18,7 +19,17 @@ batch_number="$(echo $bam_file | cut -d'/' -f2)"
 date="$(echo $bam_file | cut -d'.' -f7)"
 
 # copy over the bam and bai file
-aws s3 cp "s3://1000genomes/phase1/$bam_file" .
-aws s3 cp "s3://1000genomes/phase1/$bai_file" .
+aws s3 cp "s3://1000genomes/phase1/$bam_file" $storage_dir
+aws s3 cp "s3://1000genomes/phase1/$bai_file" $storage_dir
 
 # call mosdepth to generate output file
+cd $storage_dir
+mosdepth --by "${script_dir}/20120518.consensus_add50bp.bed" $batch_number $bam_no_dir
+
+# copy over the output file to s3 and clean up after ourselves
+aws s3 cp ${batch_number}.mosdepth.global.dist.txt s3://layerlab/exome/
+aws s3 cp ${batch_number}.per-base.bed.gz s3://layerlab/exome/
+aws s3 cp ${batch_number}.per-base.bed.gz.csi s3://layerlab/exome/
+aws s3 cp ${batch_number}.regions.bed.gz s3://layerlab/exome/
+aws s3 cp ${batch_number}.regions.bed.gz.csi s3://layerlab/exome/
+rm ${batch_number}.*
