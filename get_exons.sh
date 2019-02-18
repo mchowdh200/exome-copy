de4929d3d1671808be7912b2cc1c2fecd9cab41d
Murad <mchowdh200@gmail.com>
2019-02-18 15:25:23 -0700

    Update to get_exons script.
    Seems to work well in conjunction with gargs.  Will run even faster
    on an AWS instance with enough cores.  The command I used can be found
    in notes.org.

--- get_exons.sh
+++ get_exons.sh
@@ -2,46 +2,51 @@
 # given a set of regions a set of per-base reads (bed),
 # write each region as:
 # chrom    start    end     sig_1,sig_2,...
-regions=$(cat $1)
 
-while read -r region; do
-    # separate the columns of the bed file and get the reads
-    region_chr=$(echo $region | cut -d ' ' -f1)
-    region_start=$(echo $region | cut -d ' ' -f2)
-    region_end=$(echo $region | cut -d ' ' -f3)
+# regions=$(cat $1)
 
-    # the per base bed doesn't have 'chr' in the chromosome number
-    per_base_reads=$(tabix $2 "${region_chr: -1}:$region_start-$region_end")
+# while read -r region; do
 
-    first_read=$(echo "$per_base_reads" | head -n1)
-    last_read=$(echo "$per_base_reads" | tail -n1)
+# separate the columns of the bed file and get the reads
+# region_chr=$(echo $region | cut -d ' ' -f1)
+# region_start=$(echo $region | cut -d ' ' -f2)
+# region_end=$(echo $region | cut -d ' ' -f3)
+region_chr=$1
+region_start=$2
+region_end=$3
 
-    # if first and/or last read depth is zero skip that read
-    if [ $(echo "$first_read" | cut -f4) -eq 0 ]
-    then
-        per_base_reads=$(echo "$per_base_reads" | tail -n+2)
-    fi
+# the per base bed doesn't have 'chr' in the chromosome number
+per_base_reads=$(tabix $4 "${region_chr: -1}:$region_start-$region_end")
 
-    if [ $(echo "$last_read" | cut -f4) -eq 0 ]
-    then
-        per_base_reads=$(echo "$per_base_reads" | sed \$d)
-    fi
+first_read=$(echo "$per_base_reads" | head -n1)
+last_read=$(echo "$per_base_reads" | tail -n1)
 
-    if [ -z "$per_base_reads" ]; then
-        continue
-    fi
+# if first and/or last read depth is zero skip that read
+if [ $(echo "$first_read" | cut -f4) -eq 0 ]
+then
+    per_base_reads=$(echo "$per_base_reads" | tail -n+2)
+fi
 
-    # print the chr and the interval of the signal
-    region_chr=$(echo "$per_base_reads" | head -n1 | cut -f1)
-    region_start=$(echo "$per_base_reads" | head -n1 | cut -f2)
-    region_end=$(echo "$per_base_reads" | tail -n1 | cut -f3)
-    printf "$region_chr\t$region_start\t$region_end\t"
+if [ $(echo "$last_read" | cut -f4) -eq 0 ]
+then
+    per_base_reads=$(echo "$per_base_reads" | sed \$d)
+fi
+
+if [ -z "$per_base_reads" ]; then
+    exit 0
+fi
+
+# print the chr and the interval of the signal
+region_chr=$(echo "$per_base_reads" | head -n1 | cut -f1)
+region_start=$(echo "$per_base_reads" | head -n1 | cut -f2)
+region_end=$(echo "$per_base_reads" | tail -n1 | cut -f3)
+printf "$region_chr\t$region_start\t$region_end\t"
 
 
-    # expand the signal to match the width of the interval
-    # and add the the end of the line
-    python3 region2signal.py <<< "$per_base_reads"
+# expand the signal to match the width of the interval
+# and add the the end of the line
+python3 region2signal.py <<< "$per_base_reads"
 
 
-done <<< "$regions"
+# done <<< "$regions"
 
