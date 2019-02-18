26408267db22a4df49730a800836ed9905d9e63c
Murad <mchowdh200@gmail.com>
2019-02-18 13:55:59 -0700

    Cleaned up get_exons.sh a bit.

--- get_exons.sh
+++ get_exons.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # given a set of regions a set of per-base reads (bed),
-# write each region as $chr $start $end $(read depths, ...)
+# write each region as:
+# chrom    start    end     sig_1,sig_2,...
 regions=$(cat $1)
 
 while read -r region; do
@@ -9,8 +10,7 @@ while read -r region; do
     region_start=$(echo $region | cut -d ' ' -f2)
     region_end=$(echo $region | cut -d ' ' -f3)
 
-    # per_base_reads=$(tabix HG00096.per-base.bed.gz \
-    #     "${region_chr: -1}:$region_start-$region_end")
+    # the per base bed doesn't have 'chr' in the chromosome number
     per_base_reads=$(tabix $2 "${region_chr: -1}:$region_start-$region_end")
 
     first_read=$(echo "$per_base_reads" | head -n1)
@@ -31,24 +31,17 @@ while read -r region; do
         continue
     fi
 
+    # print the chr and the interval of the signal
     region_chr=$(echo "$per_base_reads" | head -n1 | cut -f1)
     region_start=$(echo "$per_base_reads" | head -n1 | cut -f2)
     region_end=$(echo "$per_base_reads" | tail -n1 | cut -f3)
-
     printf "$region_chr\t$region_start\t$region_end\t"
+
+
+    # expand the signal to match the width of the interval
+    # and add the the end of the line
     python3 region2signal.py <<< "$per_base_reads"
 
-
-    # while read -r reading; do
-    #     read_start=$(echo "$reading" | cut -f2)              
-    #     read_end=$(echo "$reading" | cut -f3)              
-    #     read_depth=$(echo "$reading" | cut -f4)              
-
-    #     for i in $(seq 1 $(( $read_end-$read_start ))); do
-    #         printf "$read_depth\t"
-    #     done
-    # done <<< "$per_base_reads"
-    # printf "\n"
 
 done <<< "$regions"
 
