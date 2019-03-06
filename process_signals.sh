76e95d2222dc75750f3ecbe3da0920ee6aa4d999
Murad <mchowdh200@gmail.com>
2019-03-06 09:38:49 -0700

    Update process_signals.sh

---
+++ process_signals.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SAMPLE=$1
+REGIONS_BED="$SAMPLE.regions.bed.gz"
+PERBASE_BED="$SAMPLE.per-base.bed.gz"
+PERBASE_CSI="$PERBASE_BED.csi"
+SIGNALS_BED="$SAMPLE.signals.bed"
+
+
+# Don't redownload if I don't have to
+if [ ! -f $REGIONS_BED ]; then
+    aws s3 cp "s3://layerlab/exome/$REGIONS_BED" .
+    aws s3 cp "s3://layerlab/exome/$PERBASE_BED" .
+    aws s3 cp "s3://layerlab/exome/$PERBASE_CSI" .
+fi
+
+
+# don't recalculate if I don't have to
+if [ ! -f $SIGNALS_BED ]; then
+    python3 extract_signals.py $SAMPLE > $SIGNALS_BED
+fi
+
+aws s3 cp $SIGNALS_BED "s3://layerlab/exome/signals/$SIGNALS_BED"
+
+rm "$SAMPLE.*"
+
