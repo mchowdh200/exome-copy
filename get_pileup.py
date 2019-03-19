e95a7d14446f19abfc5428c94e05a9d6385fac20
Murad <mchowdh200@gmail.com>
2019-03-18 21:44:26 -0600

    Try using stringIO to build data.

--- get_pileup.py
+++ get_pileup.py
@@ -2,19 +2,18 @@
 Demonstrate how to get the pileup sequence for a given region
 and plot the sequence/read depth as a colorcoded bar plot
 """
-
 import sys
+from io import StringIO
 import pysam
-import pandas as pd
 # import matplotlib.pyplot as plt
 
 REGIONS_BED = sys.argv[1]
 BAM_FILE = sys.argv[2]
 
+out = StringIO()
+
 with open(REGIONS_BED, 'r') as regions, \
         pysam.AlignmentFile(BAM_FILE, 'rb') as samfile:
-    data = {'chrom': [], 'start': [], 'end': [], 'signal': [], 'sequence': []}
-
     for line in regions:
         A = line.rstrip().split()
         chrom = A[0]
@@ -23,7 +22,7 @@ with open(REGIONS_BED, 'r') as regions, \
         
         signal = []
         sequence = []
-        for x in samfile.pileup(chrom, start, end):
+        for x in samfile.pileup(chrom, start, end, truncate=True):
             seq = x.get_query_sequences()
             if seq:
                 # modify this to get a distribution over bases?
@@ -32,29 +31,11 @@ with open(REGIONS_BED, 'r') as regions, \
             else:
                 signal.append(0)
                 sequence.append('_')
-        data['chrom'].append(chrom)
-        data['start'].append(start)
-        data['end'].append(end)
-        data['signal'].append(signal)
-        data['sequence'].append(''.join(sequence))
-    df = pd.DataFrame(data)
-    print(df.head())
-# with pysam.AlignmentFile(sys.argv[1], 'rb') as samfile:
-
-#     # just hardcode one of the exon locations for now
-#     signal = []
-#     sequence = []
-#     # for x in samfile.pileup('1', 861271, 861443):
-#     for x in samfile.pileup('1', 866368, 866519, max_depth=100000):
-#         # print(x.get_mapping_qualities())
-#         seq = x.get_query_sequences()
-#         print(x.pileups)
-#         if seq:
-#             signal.append(len(x.get_query_sequences()))
-#             sequence.append(x.get_query_sequences()[0])
-#         else:
-#             signal.append(0)
-#             sequence.append('_')
+        # print('{0}\t{1}\t{2}\t{3}\t{4}'.format(
+        #     chrom, start, end, signal, ''.join(sequence)))
+        out.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(
+            chrom, start, end, signal, ''.join(sequence)))
+    print(out.getvalue())
 
 #     # visualize a region by read depth color coded by base
 #     base2color = {'A': 'red', 'a': 'red', 'T': 'green',
