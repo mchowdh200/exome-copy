5ab14229a01a5fc05d403d32eaf2c60b05225bb5
Murad Chowdhury <much8161@fiji-2.colorado.edu>
2019-04-12 16:25:14 -0600

    update modify process bams to only get SV's if the file doesn't exist yet.

--- process_bams.sh
+++ process_bams.sh
@@ -8,28 +8,24 @@ DATA_DIR=$2
 SAMPLE=$(echo $BAM_FILE_WITH_DIR | cut -d '/' -f2 )
 BAM_FILE=$(echo $BAM_FILE_WITH_DIR | cut -d '/' -f4)
 
-# if [ -f "$DATA_DIR/labels/$SAMPLE.del.bed" ] || [ -f "$DATA_DIR/labels/$SAMPLE.dup.bed" ]; then
-    # get bam/bai only if we have corresponding SV bed files
-    # aws s3 cp "s3://1000genomes/phase1/$BAM_FILE_WITH_DIR" $DATA_DIR
-    # aws s3 cp "s3://1000genomes/phase1/$BAM_FILE_WITH_DIR.bai" $DATA_DIR
+# If we've got the BAM's and if we've not already extracted SV's
+if [ -f "$DATA_DIR/$BAM_FILE" ] && [ -f "$DATA_DIR/$BAM_FILE.bai" ]; then
 
-# get pileups sequence from bam files
-if [ -f "$DATA_DIR/labels/$SAMPLE.del.bed" ]; then
-    python get_pileup.py \
-        "$DATA_DIR/labels/$SAMPLE.del.bed" \
-        "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.del.bed"
+    # get pileups sequence from bam files
+    if [ -f "$DATA_DIR/labels/$SAMPLE.del.bed" ] && \
+       [ ! -f "$DATA_dir/pileups/$SAMPLE.pileups.del.bed" ]; then
+        python get_pileup.py \
+            "$DATA_DIR/labels/$SAMPLE.del.bed" \
+            "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.del.bed"
+    fi
+
+    if [ -f "$DATA_DIR/labels/$SAMPLE.dup.bed" ] && \
+       [ ! -f "$DATA_DIR/pileups/$SAMPLE.pileups.dup.bed" ]; then
+        python get_pileup.py \
+            "$DATA_DIR/labels/$SAMPLE.dup.bed" \
+            "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.dup.bed"
+    fi
+
 fi
-
-if [ -f "$DATA_DIR/labels/$SAMPLE.dup.bed" ]; then
-    python get_pileup.py \
-        "$DATA_DIR/labels/$SAMPLE.dup.bed" \
-        "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.dup.bed"
-fi
-
-# clean up
-# rm "$DATA_DIR/$BAM_FILE"
-# rm "$DATA_DIR/$BAM_FILE.bai"
-
-# fi
 
 
