09c1a927381fbc0fe56d203c62d5f2e32ace6e84
Murad Chowdhury <much8161@fiji-1.colorado.edu>
2019-04-15 13:25:37 -0600

    Reduce cores to 64 in process_bams.
    For some reason, spreading across nodes caused excessive IO waiting.

--- count_svs.sh
+++ count_svs.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DATA_DIR=$1
+
 count=$((0))
 
 for SAMPLE in $(cat data/samples.txt); do
-    if [ -f "data/labels/$SAMPLE.del.bed" ]; then
-        count=$((count + $(wc -l < "data/labels/$SAMPLE.del.bed")))
+    if [ -f "$DATA_DIR/labels/$SAMPLE.del.bed" ]; then
+        count=$((count + $(wc -l < "$DATA_DIR/labels/$SAMPLE.del.bed")))
     fi
 
-    if [ -f "data/labels/$SAMPLE.dup.bed" ]; then
-        count=$((count + $(wc -l < "data/labels/$SAMPLE.dup.bed")))
+    if [ -f "$DATA_DIR/labels/$SAMPLE.dup.bed" ]; then
+        count=$((count + $(wc -l < "$DATA_DIR/labels/$SAMPLE.dup.bed")))
     fi
 done
 
