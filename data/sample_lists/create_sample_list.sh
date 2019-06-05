6d353a37ebeaf481115af6dd4260a0928f28d21a
Murad <mchowdh200@gmail.com>
2019-06-05 14:39:25 -0600

    Modified create_sample_list to shuffle the samples

--- data/sample_lists/create_sample_list.sh
+++ data/sample_lists/create_sample_list.sh
@@ -4,6 +4,9 @@ SAMPLE_INDEX="./phase1.exome.alignment.index"
 VCF="../VCF/ALL.wgs.mergedSV.v8.20130502.svs.genotypes.vcf.gz"
 
 # only keep samples sequenced with illumina then get the sample ID from index
-# and intersect with samples that exist in the VCF
+# and intersect with samples that exist in the VCF.  Let's shuffle afterwards
+# for good measure
 ILLUMINA_SAMPLES=$(grep "illumina" $SAMPLE_INDEX | cut -d '/' -f2)
-sort <(echo "$ILLUMINA_SAMPLES") <(bcftools query -l $VCF) | uniq -d > all_samples.txt
+sort <(echo "$ILLUMINA_SAMPLES") \
+     <(bcftools query -l $VCF) | \
+     uniq -d | shuf > all_samples.txt
