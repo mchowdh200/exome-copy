0e868171775ce49e0e391c23a19407f0d4613733
Murad Chowdhury <much8161@fiji-1.colorado.edu>
2019-06-06 11:50:47 -0600

    Changed BAM processing to work with train/test/split.

--- get_pileups/process_bams.sh
+++ get_pileups/process_bams.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# input bam file and output directory
-# TODO modify this script to match the command line args
+# Where to find data and where to put results
 BAM_FILE_WITH_DIR=$1
 DATA_DIR=$2
+LABEL_DIR=$3
+OUT_DIR=$4
 
 # get the sample number and bam filename from the file index
 SAMPLE=$(echo $BAM_FILE_WITH_DIR | cut -d '/' -f2 )
@@ -13,18 +14,18 @@ BAM_FILE=$(echo $BAM_FILE_WITH_DIR | cut -d '/' -f4)
 if [ -f "$DATA_DIR/$BAM_FILE" ] && [ -f "$DATA_DIR/$BAM_FILE.bai" ]; then
 
     # get pileups sequence from bam files
-    if [ -f "$DATA_DIR/labels/$SAMPLE.del.bed" ] && \
-       [ ! -f "$DATA_DIR/pileups/$SAMPLE.pileups.del.bed" ]; then
+    if [ -f "$LABEL_DIR/$SAMPLE.del.bed" ] && \
+       [ ! -f "$OUT_DIR/$SAMPLE.pileups.del.bed" ]; then
         python get_pileup.py \
-            "$DATA_DIR/labels/$SAMPLE.del.bed" \
-            "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.del.bed"
+            "$LABEL_DIR/$SAMPLE.del.bed" \
+            "$DATA_DIR/$BAM_FILE" > "$OUT_DIR/$SAMPLE.pileups.del.bed"
     fi
 
-    if [ -f "$DATA_DIR/labels/$SAMPLE.dup.bed" ] && \
-       [ ! -f "$DATA_DIR/pileups/$SAMPLE.pileups.dup.bed" ]; then
+    if [ -f "$LABEL_DIR/$SAMPLE.dup.bed" ] && \
+       [ ! -f "$OUT_DIR/$SAMPLE.pileups.dup.bed" ]; then
         python get_pileup.py \
-            "$DATA_DIR/labels/$SAMPLE.dup.bed" \
-            "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.dup.bed"
+            "$LABEL_DIR/$SAMPLE.dup.bed" \
+            "$DATA_DIR/$BAM_FILE" > "$OUT_DIR/$SAMPLE.pileups.dup.bed"
     fi
 
 fi
--- get_pileups/sample_nonsv.sh
+++ get_pileups/sample_nonsv.sh
@@ -3,6 +3,8 @@
 # input bam file and output directory
 BAM_FILE_WITH_DIR=$1
 DATA_DIR=$2
+LABEL_DIR=$3
+OUT_DIR=$4
 
 # number of regions to sample per bed file
 N=100
@@ -16,14 +18,14 @@ BAM_FILE=$(echo $BAM_FILE_WITH_DIR | cut -d '/' -f4)
 if [ -f "$DATA_DIR/$BAM_FILE" ] && [ -f "$DATA_DIR/$BAM_FILE.bai" ]; then
     
     # get nonsv pileups from bam file
-    if [ -f "$DATA_DIR/labels/$SAMPLE.nosv.bed" ] && \
-       [ ! -f "$DATA_DIR/pileups/$SAMPLE.pileups.nosv.bed" ]; then
+    if [ -f "$LABEL_DIR/$SAMPLE.nosv.bed" ] && \
+       [ ! -f "$OUT_DIR/$SAMPLE.pileups.nosv.bed" ]; then
         # shuffle the bed file, then take the first N
-        shuf "$DATA_DIR/labels/$SAMPLE.nosv.bed" | head -$N > "$DATA_DIR/labels/$SAMPLE.subset.bed"
+        shuf "$LABEL_DIR/$SAMPLE.nosv.bed" | head -$N > "$LABEL_DIR/$SAMPLE.subset.bed"
         python get_pileup_nogtype.py \
-            "$DATA_DIR/labels/$SAMPLE.subset.bed" \
-            "$DATA_DIR/$BAM_FILE" > "$DATA_DIR/pileups/$SAMPLE.pileups.nosv.bed"
+            "$LABEL_DIR/$SAMPLE.subset.bed" \
+            "$DATA_DIR/$BAM_FILE" > "$OUT_DIR/$SAMPLE.pileups.nosv.bed"
 
-        rm "$DATA_DIR/labels/$SAMPLE.subset.bed"
+        rm "$LABEL_DIR/$SAMPLE.subset.bed"
     fi
 fi
